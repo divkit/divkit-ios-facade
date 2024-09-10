19d54733552f3eb80be4578e810af6cfca9b8094
Roman Solyah <booster@yandex-team.ru>
2024-09-10 20:54:25 +0300

    Initial OpenSource export

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+  name: "DivKitBinaryCompatibilityFacade",
+  platforms: [
+    .iOS(.v13),
+  ],
+  products: [
+    .library(
+      name: "DivKitBinaryCompatibilityFacade",
+      targets: ["DivKitBinaryCompatibilityFacade"]
+    ),
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/divkit/divkit-ios.git",
+      from: Version(stringLiteral: "30.9.0")
+    )
+  ],
+  targets: [
+    .target(
+      name: "DivKitBinaryCompatibilityFacade",
+      dependencies: [
+        .product(name: "DivKit", package: "divkit-ios")
+      ],
+      path: "DivKitBinaryCompatibilityFacade",
+      swiftSettings: [
+        .unsafeFlags(["-enable-library-evolution"]),
+      ]
+    ),
+  ]
+)
